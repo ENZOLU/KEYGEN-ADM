ed325033fe66e21a14e09729691608a9f21c2dad
ENZOLU <35827490+ENZOLU@users.noreply.github.com>
2018-12-22 20:37:27 -0300

    Update gerar.sh

--- gerador/gerar.sh
+++ gerador/gerar.sh
@@ -125,7 +125,7 @@ valuekey="$(date | md5sum | head -c10)"
 valuekey+="$(echo $(($RANDOM*10))|head -c 5)"
 fun_list "$valuekey"
 keyfinal=$(ofus "$IP:8888/$valuekey/$LIST")
-echo -e "KEY: $keyfinal\ngenrada!"
+echo -e "KEY: $keyfinal\ngenerada!"
 echo -e "$BARRA"
 read -p "Enter para finalizar"
 }
