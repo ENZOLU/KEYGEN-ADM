0d165ceea5e9c915039de2e5d092bca38ee029e2
ENZOLU <35827490+ENZOLU@users.noreply.github.com>
2018-12-26 23:39:56 -0300

    Update gerar.sh

--- gerador/gerar.sh
+++ gerador/gerar.sh
@@ -20,7 +20,7 @@ rm ${SCPT_DIR}/*.x.c &> /dev/null
 INSTA_ARQUIVOS="ADMVPS.zip"
 DIR="/etc/http-shell"
 LIST="lista-arq"
-GENLIST="/etc/GENERADOR/GERADOR"
+GENLIST="gerar.sh http-server.py menu PGet.py ports.sh ADMbot.sh message.txt usercodes sockspy.sh POpen.py PPriv.py PPub.py PDirect.py speedtest.py speed.sh utils.sh dropbear.sh apacheon.sh openvpn.sh shadowsocks.sh ssl.sh squid.sh fai2ban.sh gestor.sh vnc dados.sh anti-ddos.sh un-ddos.sh crear-demo GENERADOR_BIN.sh MasterBin.sh paysnd.sh payySND.sh ultrahost"
 }
 meu_ip () {
 MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
