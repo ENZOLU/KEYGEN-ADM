8ff21f0713cd6b0395b39e4d82d230ed0e1cc1d7
ENZOLU <35827490+ENZOLU@users.noreply.github.com>
2018-12-27 14:48:57 -0300

    Update gerar.sh

--- gerador/gerar.sh
+++ gerador/gerar.sh
@@ -179,7 +179,7 @@ echo "$MSGNEW" > ${SCPT_DIR}/message.txt
 echo -e "$BARRA"
 }
 atualizar_geb () {
-wget -q $HOME/instger.sh https://www.dropbox.com/s/dp5064knooelcos/actgenerador.sh?dl=0
+wget -q $HOME/instger.sh https://raw.githubusercontent.com/ENZOLU/GENERADOR-NEW-ULTIMATE-ORIGINAL-1/master/actgenerador.sh
 chmod 777 $HOME/instger.sh
 cd $HOME
 ./instger.sh
