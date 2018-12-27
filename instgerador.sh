3acb50cabb9db736a874dccec86554dce49e07f8
ENZOLU <35827490+ENZOLU@users.noreply.github.com>
2018-12-27 04:50:04 -0300

    Update instgerador.sh

--- instgerador.sh
+++ instgerador.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 IVAR="/etc/http-instas"
 SCPT_DIR="/etc/SCRIPT"
-SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0VOWk9MVS9HRU5FUkFET1ItTkVXLVVMVElNQVRFLU9SSUdJTkFMLTEvbWFzdGVyL2dlcmFkb3I="
-SUB_DOM='base64 -d'
 rm $(pwd)/$0
 ofus () {
 unset txtofus
@@ -38,6 +36,15 @@ mv -f $HOME/$1 ${ARQ}/$1
 chmod +x ${ARQ}/$1
 }
 echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;36m--------------------KEY GENERATOR BY MARCHNICK--------------\033[0m"
+read -p "INTRODUZCA SU KEY DE INSTALACIÓN: " Key
+echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+[[ ! $Key ]] && {
+echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;33mKey inválida!"
+echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+exit
+}
 meu_ip () {
 MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 MIP2=$(wget -qO- ipv4.icanhazip.com)
@@ -45,47 +52,34 @@ MIP2=$(wget -qO- ipv4.icanhazip.com)
 echo "$IP" > /usr/bin/vendor_code
 }
 meu_ip
-echo -e "\033[1;33mDescargando archivos... "
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "\033[1;33mVerificando key... "
 cd $HOME
-REQUEST=$(echo $SCPresq|$SUB_DOM)
-wget -O "$HOME/lista-arq" ${REQUEST}/GERADOR > /dev/null 2>&1
+wget -O "$HOME/lista-arq" $(ofus "$Key")/$IP > /dev/null 2>&1
+IP=$(ofus "$Key" | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 sleep 1s
 [[ -e $HOME/lista-arq ]] && {
+REQUEST=$(ofus "$Key" |cut -d'/' -f2)
 for arqx in `cat $HOME/lista-arq`; do
 echo -ne "\033[1;33mDescargando archivo: \033[1;31m[$arqx] "
-wget -O $HOME/$arqx ${REQUEST}/${arqx} > /dev/null 2>&1 && {
-echo -e "\033[1;31m- \033[1;32mRecibido con exito!"
+wget -O $HOME/$arqx ${IP}:81/${REQUEST}/${arqx} > /dev/null 2>&1 && echo -e "\033[1;31m- \033[1;32mRecibido con éxito!" || echo -e "\033[1;31m- \033[1;31mFalla (no recibido!)"
 [[ -e $HOME/$arqx ]] && veryfy_fun $arqx
-} || echo -e "\033[1;31m- \033[1;31mFalla (no recibido!)"
 done
-[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/GENERADOR-NEW-ULTIMATE-ORIGINAL/master/Install/trans &> /dev/null
-[[ -e /bin/http-server.py ]] && mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
-[[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || apt-get install bc -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "screen"|head -1) ]] || apt-get install screen -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "nano"|head -1) ]] || apt-get install nano -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] || apt-get install curl -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] || apt-get install netcat -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "apache2"|head -1) ]] || apt-get install apache2 -y &>/dev/null
+[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://www.dropbox.com/s/l6iqf5xjtjmpdx5/trans?dl=0 &> /dev/null
+mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
+apt-get install bc -y &>/dev/null
+apt-get install screen -y &>/dev/null
+apt-get install nano -y &>/dev/null
+apt-get install curl -y &>/dev/null
+apt-get install netcat -y &>/dev/null
+apt-get install apache2 -y &>/dev/null
 sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
 service apache2 restart > /dev/null 2>&1 &
 IVAR2="/etc/key-gerador"
 echo "$Key" > $IVAR2
-mkdir /etc/GENERADOR
-chmod 777 /etc/GENERADOR
-cp /etc/SCRIPT/* /etc/GENERADOR
-cp /usr/bin/gerar.sh /etc/GENERADOR
-cp $HOME/lista-arq /etc/GENERADOR
 rm $HOME/lista-arq
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
-echo "/usr/bin/gerar.sh" > /usr/bin/gerar && chmod +x /usr/bin/gerar
-echo -e "\033[1;33m Perfecto, utilize el comando \033[1;31mgerar.sh o gerar \033[1;33mpara administrar sus keys y
- actualizar la base del servidor"
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
 } || {
 echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
-echo -e "\033[1;33mKey Invalida!"
+echo -e "\033[1;33mKey inválida!"
 echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
 }
-echo -ne "\033[0m"
-apt-get install netcat -y &>/dev/null
+echo -ne "\033[0m"1
